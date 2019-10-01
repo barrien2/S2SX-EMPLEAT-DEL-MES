4df476c615e77c7c21cff1379df56f7334fb674f
barrien2 <xavib99@gmail.com>
2019-10-01 18:01:57 +0200

    No mostrar imatge si no s'ha penjat

--- insignies.php
+++ insignies.php
@@ -38,46 +38,52 @@ th {
 
 <div>
   <table class="centerTable" style="overflow-x:auto;">
-      <tr>
-        <th>nom</th>
-        <th>imatge</th>
-        <th>data limit</th>
-        <th>limit</th>
-        <th>descripcio</th>
-        <th>actiu</th>
-      </tr>
-      <?php
+      
+    <tr>
+      <th>nom</th>
+      <th>imatge</th>
+      <th>data limit</th>
+      <th>limit</th>
+      <th>descripcio</th>
+      <th>actiu</th>
+    </tr>
+    <?php
 
-$target_dir = "uploads/";
-if(is_dir($target_dir)){
-  $target_file = $target_dir . basename($_FILES["image"]["name"]);
-  $uploadOk = 1;
-  $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-  // Check if image file is a actual image or fake image
-  if(isset($_FILES["image"]["name"])) {
-      $check = getimagesize($_FILES["image"]["tmp_name"]);
-      if($check !== false) {
-          move_uploaded_file($_FILES["image"]['tmp_name'], $target_file);
-          $uploadOk = 1;
-      } else {
-          
+      $target_dir = "uploads/";
+      if(is_dir($target_dir)){
+        $target_file = $target_dir . basename($_FILES["image"]["name"]);
+        $uploadOk = 1;
+        $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+        // Check if image file is a actual image or fake image
+        if(isset($_FILES["image"]["name"]) && $_FILES["image"]["name"] != '') {
+            $check = getimagesize($_FILES["image"]["tmp_name"]);
+            if($check !== false) {
+                move_uploaded_file($_FILES["image"]['tmp_name'], $target_file);
+                $uploadOk = 1;
+            } 
+        }
+        else {
+                
           $uploadOk = 0;
       }
-  }
-}
+      }
 
-        
-          echo "<tr>";
-          echo "<td>".$_POST["nom"]."</td>";
-          echo "<td>".' <img src = "uploads/'.$_FILES["image"]["name"].'" height="150">';
-          echo "<td>".$_POST['datalimit']."</td>";
-          echo "<td>".$_POST['limit']."</td>";
-          echo "<td>".$_POST['descripcio']."</td>";
-          echo "<td>".$_POST['actiu']."</td>";
-          echo "</tr>";
-        
-        
-      ?>
+          
+      echo "<tr>";
+      echo "<td>".$_POST["nom"]."</td>";
+      if($uploadOk == 1){
+        echo "<td>".' <img src = "uploads/'.$_FILES["image"]["name"].'" height="150">';
+      }else{
+        echo "<td> </td>";
+      }
+      echo "<td>".$_POST['datalimit']."</td>";
+      echo "<td>".$_POST['limit']."</td>";
+      echo "<td>".$_POST['descripcio']."</td>";
+      echo "<td>".$_POST['actiu']."</td>";
+      echo "</tr>";
+          
+          
+    ?>
   </table>
 </div>
 </html>
