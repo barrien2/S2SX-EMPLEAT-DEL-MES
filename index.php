b924720d6a250e33d3b1c8f7d546db65f9237ab7
barrien2 <xavib99@gmail.com>
2019-10-03 17:37:30 +0200

    Reindent

--- index.php
+++ index.php
@@ -3,38 +3,38 @@
 ?>
 <html>
 <head>
-<title>ARASI</title>
-<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-<link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
-<script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-<style>
-table {
-  border-collapse: collapse;
-  width: 80%;
-  align: center;
-}
+  <title>ARASI</title>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+  <style>
+    table {
+      border-collapse: collapse;
+      width: 80%;
+      align: center;
+    }
 
-th, td {
-  text-align: left;
-  padding: 8px;
-}
+    th, td {
+      text-align: left;
+      padding: 8px;
+    }
 
-tr:nth-child(even){background-color: #f2f2f2}
+    tr:nth-child(even){background-color: #f2f2f2}
 
-th {
-  background-color: #4CAF50;
-  color: white;
-}
-.centerTable { margin: 20px auto; }
+    th {
+      background-color: #4CAF50;
+      color: white;
+    }
+    .centerTable { margin: 20px auto; }
 
-</style>
+  </style>
 </head>
 
 <body>
 
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <?php
-      include("header.php");
+    include("header.php");
     ?>
     <main class="mdl-layout__content">
       <div class="page-content">
@@ -43,5 +43,4 @@ th {
     </main>
   </div>
 </body>
-</div>
 </html>
--- insignies.php
+++ insignies.php
@@ -1,84 +1,96 @@
 <?php
 
-  $target_dir = "uploads/";
-  if(is_dir($target_dir)){
-    $target_file = $target_dir . basename($_FILES["image"]["name"]);
-    $uploadOk = 1;
-    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+$target_dir = "uploads/";
+if(is_dir($target_dir)){
+  $target_file = $target_dir . basename($_FILES["image"]["name"]);
+  $uploadOk = 1;
+  $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
     // Check if image file is a actual image or fake image
-    if(isset($_FILES["image"]["name"]) && $_FILES["image"]["name"] != '') {
+  if(isset($_FILES["image"]["name"]) && $_FILES["image"]["name"] != '') {
 
-      $check = getimagesize($_FILES["image"]["tmp_name"]);
-      
-      if($check !== false) {
-        move_uploaded_file($_FILES["image"]['tmp_name'], $target_file);
-        $uploadOk = 1;
-      } 
-    }
-    else {
-            
-      $uploadOk = 0;
-    }
+    $check = getimagesize($_FILES["image"]["tmp_name"]);
+    
+    if($check !== false) {
+      move_uploaded_file($_FILES["image"]['tmp_name'], $target_file);
+      $uploadOk = 1;
+    } 
   }
+  else {
+    
+    $uploadOk = 0;
+  }
+}
 ?>
 
 <html>
 <head>
-<title>ARASI</title>
-<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-<link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
-<script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+  <title>ARASI</title>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
 </head>
 <style>
-table {
-  border-collapse: collapse;
-  width: 80%;
-  align: center;
-}
+  table {
+    border-collapse: collapse;
+    width: 80%;
+    align: center;
+  }
 
-th, td {
-  text-align: left;
-  padding: 8px;
-}
+  th, td {
+    text-align: left;
+    padding: 8px;
+  }
 
-tr:nth-child(even){background-color: #f2f2f2}
+  tr:nth-child(even){background-color: #f2f2f2}
 
-th {
-  background-color: #4CAF50;
-  color: white;
-}
-.centerTable { margin: 20px auto; }
+  th {
+    background-color: #4CAF50;
+    color: white;
+  }
+  .centerTable { margin: 20px auto; }
 
 </style>
 
-<div>
-  <table class="centerTable" style="overflow-x:auto;">
-      
-    <tr>
-      <th>nom</th>
-      <th>imatge</th>
-      <th>data limit</th>
-      <th>limit</th>
-      <th>descripcio</th>
-      <th>actiu</th>
-    </tr>
-<?php
-          
-      echo "<tr>";
-      echo "<td>".$_POST["nom"]."</td>";
-      if($uploadOk == 1){
-        echo "<td>".' <img src = "uploads/'.$_FILES["image"]["name"].'" height="150">';
-      }else{
-        echo "<td> </td>";
-      }
-      echo "<td>".$_POST['datalimit']."</td>";
-      echo "<td>".$_POST['limit']."</td>";
-      echo "<td>".$_POST['descripcio']."</td>";
-      echo "<td>".$_POST['actiu']."</td>";
-      echo "</tr>";
-          
-          
+<body>
+
+  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <?php
+    include("header.php");
     ?>
-  </table>
-</div>
+    <main class="mdl-layout__content">
+      <div class="page-content">
+        <div>
+          <table class="centerTable" style="overflow-x:auto;">
+            
+            <tr>
+              <th>nom</th>
+              <th>imatge</th>
+              <th>data limit</th>
+              <th>limit</th>
+              <th>descripcio</th>
+              <th>actiu</th>
+            </tr>
+            <?php
+            
+            echo "<tr>";
+            echo "<td>".$_POST["nom"]."</td>";
+            if($uploadOk == 1){
+              echo "<td>".' <img src = "uploads/'.$_FILES["image"]["name"].'" height="150">';
+            }else{
+              echo "<td> </td>";
+            }
+            echo "<td>".$_POST['datalimit']."</td>";
+            echo "<td>".$_POST['limit']."</td>";
+            echo "<td>".$_POST['descripcio']."</td>";
+            echo "<td>".$_POST['actiu']."</td>";
+            echo "</tr>";
+            
+            
+            ?>
+          </table>
+        </div>
+      </div>
+    </main>
+  </div>
+</body>
 </html>
